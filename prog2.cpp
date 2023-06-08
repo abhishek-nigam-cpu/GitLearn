e2dfe057c98c5f70b4ff0d112248179d7bc3d56d
Abhishek Nigam <abhisheknigam17062001@gmail.com>
2023-06-08 11:19:41 +0530

    added code for checking prime number

--- prog2.cpp
+++ prog2.cpp
@@ -1,1 +1,23 @@
-#include<iostream>
+#include <iostream>
+using namespace std;
+bool checkPrime(int num)
+{
+    if (num <= 1)
+    {
+        return false;
+    }
+    for (int i = 2; i * i <= num; ++i)
+    {
+        if (num % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+int main()
+{
+    cout<<checkPrime(5)<<endl;
+    cout<<checkPrime(10)<<endl;
+    return 0;
+}
